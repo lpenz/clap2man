7e5dfa60798d07b6cc99b56bd9c8c0fe4e011544
Leandro Lisboa Penz <lpenz@lpenz.org>
2026-03-28 15:55:24 +0000

    test: add a simple integration test

---
+++ tests/integration_test.rs
@@ -0,0 +1,63 @@
+// Copyright (C) 2026 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+use clap::{Arg, Command};
+use clap2man::Manual;
+
+#[test]
+fn test_integration() {
+    let cmd = Command::new("test-app")
+        .version("1.2.3")
+        .author("John Doe <john@doe.com>")
+        .about("A test application for clap2man")
+        .long_about("This is a longer description of the test application. It should show up in the description section.")
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .help("Enable verbose mode")
+                .action(clap::ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .help("The configuration file to use")
+                .num_args(1),
+        )
+        .arg(
+            Arg::new("input")
+                .help("The input file")
+                .required(true)
+                .index(1),
+        )
+        .subcommand(
+            Command::new("sub")
+                .about("A subcommand")
+                .arg(Arg::new("sub-flag").short('s').long("sub-flag")),
+        );
+
+    let manual = Manual::from(&cmd);
+    let manpage: man::Manual = manual.into();
+    let rendered = manpage.render();
+    println!("{}", rendered);
+
+    assert!(rendered.contains("test\\-app"));
+    assert!(rendered.contains("A test application for clap2man"));
+    assert!(rendered.contains("John Doe <john@doe.com>"));
+    assert!(rendered.contains("Enable verbose mode"));
+    assert!(rendered.contains("The configuration file to use"));
+    assert!(rendered.contains("input"));
+    assert!(rendered.contains("ARGUMENTS"));
+    assert!(rendered.contains("The input file"));
+    assert!(rendered.contains("SUBCOMMANDS"));
+    assert!(rendered.contains("sub"));
+    assert!(rendered.contains("A subcommand"));
+
+    // Check if flags are correctly formatted in ROFF
+    assert!(rendered.contains("\\-v"));
+    assert!(rendered.contains("\\-\\-verbose"));
+    assert!(rendered.contains("\\-c"));
+    assert!(rendered.contains("\\-\\-config"));
+}
